ccf1596138f1863a41c790934b91ac24500ca462
leoncampean <leoncampean90@gmail.com>
2021-09-16 16:04:20 +0200

    Changing the component name, because its enough material for 1 component

--- website-cms/src/App.vue
+++ website-cms/src/App.vue
@@ -13,7 +13,7 @@
 <script>
 import TitleComponent from './components/TitleComponent.vue'
 import Text from './components/Text.vue'
-import AdaugareButon from './components/ButoaneSearchBar.vue'
+import AdaugareButon from './components/Butoane.vue'
 
 
 export default {
